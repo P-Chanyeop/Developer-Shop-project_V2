30a1e8fd120fc12d780ebe26489187afcfcec827
P-Chanyeop <qkrcksduq1gh@naver.com>
2024-03-18 11:23:15 +0900

    first commit

---
+++ src/main/java/com/example/develope_mall/DevelopeMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.develope_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopeMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopeMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/develope_mall/DevelopeMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.develope_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopeMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
